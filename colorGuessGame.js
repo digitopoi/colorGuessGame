93c70e25aee5079132009de61259a0c2e63ca4de
digitopoi <hubo85@gmail.com>
2017-02-20 12:46:28 -0600

    Add game logic

--- colorGuessGame.js
+++ colorGuessGame.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,13 +23,25 @@ for (var i = 0; i < squares.length; i++) {
 		var clickedColor = this.style.background;
 		//	Compare color to picked color
 		if (clickedColor === pickedColor) {
-			alert("Correct");
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
 		} else {
-			alert("Wrong");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
-
 }
 
+function changeColors(color) {
+	//	Loop through all squares
+	for (var i = 0; i < squares.length; i++) {
+		//	Change each color to match given color
+		squares[i].style.background = color;
+	}
+}
 
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
 				
